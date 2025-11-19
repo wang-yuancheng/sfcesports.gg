266622b0cfcdb95be38b908ffa41e74fd84d8cd8
wang-yuancheng <yuanchengntu@gmail.com>
2025-11-19 08:48:31 +0800

    Create 1v1s4 page

--- website/app/(root)/events/1v1s4/page.tsx
+++ website/app/(root)/events/1v1s4/page.tsx
@@ -1,9 +1,83 @@
-import React from 'react'
+"use client";
 
-const tdms4 = () => {
+import PageHeaderImage from "@/components/global/PageHeaderImage";
+import tdms4Banner from "@/assets/pictures/1v1s4thumbnail.jpg";
+import { useMemo, useState } from "react";
+import { useYoutubeViews } from "@/hooks/useYoutubeViews";
+import { VideoItem } from "@/lib/types";
+import { tdms4SemiFinals, tdms4Media, videos } from "@/data/events/1v1s4";
+import VideoCard from "@/components/home/VideoCard";
+import VideoModal from "@/components/home/VideoModal";
+import MediaGallery from "@/components/home/MediaGallery";
+import TdmLeaderboard from "@/components/leaderboards/leaderboards/TdmLeaderboard";
+
+
+export default function tdms4() {
+  const [active, setActive] = useState<VideoItem | null>(null);
+  const ids = useMemo(() => videos.map((v) => v.id), []);
+  const viewsMap = useYoutubeViews(ids);
+
   return (
-    <div>tdms4</div>
-  )
-}
+    <div className="mb-16">
+      <PageHeaderImage desktopSrc={tdms4Banner} />
+      <div className="max-w-[1600px] mx-auto px-[clamp(1rem,4vw,4rem)]">
+        <div className="md:mx-[clamp(1rem,6vw,20rem)]">
+          <div className="flex flex-col gap-10">
+            <section className="flex flex-col gap-4">
+              <h1 className="mt-10 text-xl xs:text-2xl sm:text-3xl md:text-4xl font-druk uppercase text-center  leading-tight">
+                Shibe's 1v1 Tournament: Season 4
+              </h1>
+              <p className="text-gray-600 font-[400] text-base text-justify">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+            </section>
 
-export default tdms4
+            <section>
+              <div className="flex flex-col gap-3">
+                <h2 className="text-xl md:text-2xl font-druk uppercase">
+                  Standings
+                </h2>
+                <TdmLeaderboard rows={tdms4SemiFinals} />
+              </div>
+            </section>
+
+            <section>
+              <div className="flex flex-col gap-3">
+                <h2 className="text-xl md:text-2xl font-druk uppercase">
+                  Vods
+                </h2>
+                <div className="grid grid-cols-1 gap-5 md:grid-cols-9">
+                  {videos.map((v) => (
+                    <div key={v.id} className="md:col-span-3 ">
+                      <VideoCard
+                        item={v}
+                        onOpen={setActive}
+                        viewCount={viewsMap[v.id]}
+                      />
+                    </div>
+                  ))}
+                </div>
+
+                <VideoModal
+                  open={Boolean(active)}
+                  title={active?.title ?? ""}
+                  videoId={active?.id ?? ""}
+                  onClose={() => setActive(null)}
+                />
+              </div>
+            </section>
+            <section>
+              <MediaGallery media={tdms4Media} hideArrows />
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
