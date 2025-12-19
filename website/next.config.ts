ffa3b1eb3847bd0fa590b49f2789570c0502c622
wang-yuancheng <yuanchengntu@gmail.com>
2025-12-20 02:23:01 +0800

    Whitelist external domains

--- website/next.config.ts
+++ website/next.config.ts
@@ -7,6 +7,30 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "wjcgjfsmgyrphkutujnk.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh4.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "platform-lookaside.fbsbx.com",
+      },
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "abs.twimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+      },
     ],
   },
 };
